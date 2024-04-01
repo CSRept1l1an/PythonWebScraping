63196aa8ce46e44c7291691f5aa239cff43f871e
CSRept1l1an <csyusifovtelman@gmail.com>
2024-04-01 12:55:08 +0400

    NVD test finished

--- main.py
+++ main.py
@@ -11,8 +11,12 @@ vector = doc.find('span', {'data-testid': 'vuln-cvss3-nist-vector'})
 baseScore = doc.find('a', {'data-testid': 'vuln-cvss3-panel-score'})
 hyperlinks = doc.find('table', {'data-testid': 'vuln-hyperlinks-table'})
 links = hyperlinks.find_all('a')
+quickInfo = doc.find('div', {'class': 'bs-callout bs-callout-info'})
+
+print(descriptions.text)
+print(baseScore.text)
+print(vector.text)
+print(quickInfo.text)
+
 for link in links:
     print(link.text.strip())
-
-quickInfo = doc.find('div', {'class': 'bs-callout bs-callout-info'})
-print(quickInfo.text.strip())
